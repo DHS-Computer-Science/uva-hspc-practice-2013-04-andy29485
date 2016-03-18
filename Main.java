beb167b40c4fd27660302558f13df3df9d81eca5
Noah <AndriyZasypkin@gmail.com>
2016-03-17 20:07:33 -0400

    Added Solution

--- Main.java
+++ Main.java
@@ -1,9 +1,50 @@
-//TODO: <name>
-//TODO: <date>
+//Andriy Zasypkin
+//2016-03-17
 //Practice 2013 - 04: Morse Code
 
-public class  {
+import java.util.*
+
+public class Main {
+  Map<String,String> = MORSE_MAP = new HashMap<String,String>() {{
+    put(".-",   "A");
+    put("-...", "B");
+    put("-.-.", "C");
+    put("..-",  "D");
+    put(".",    "E");
+    put("..-.", "F");
+    put("--.",  "G");
+    put("....", "H");
+    put("..",   "I");
+    put(".---", "J");
+    put("-.-",  "K");
+    put(".-..", "L");
+    put("--",   "M");
+    put("-.",   "N");
+    put("---",  "O");
+    put(".--.", "P");
+    put("--.-", "Q");
+    put(".-.",  "R");
+    put("...",  "S");
+    put("-",    "T");
+    put("..-",  "U");
+    put("...-", "V");
+    put(".--",  "W");
+    put("-..-", "X")
+    put("-.--", "Y");
+    put("--..", "Z");
+  }};
+
   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int nCases = scan.nextInt();
+
+    for(int i=0; i<nCases; i++) {
+      String[] codes = scan.nextLine().split("\\s+");
+      for(String code : codes) {
+        System.out.print(MORSE_MAP.get(code));
+      }
+      System.out.println();
+    }
 
     System.exit(0);
   }
